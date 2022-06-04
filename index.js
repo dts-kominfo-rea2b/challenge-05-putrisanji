098cf7d31f7e928a71182a64cf6cbad1bef00d6c
putri <putrisanjiwanidewi@gmail.com>
2022-06-04 20:58:39 +0900

    submit challenge

--- index.js
+++ index.js
@@ -6,25 +6,37 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (names, shortData) => {
+    const newData = shortData(names);
+    const result = [];
+    for (let i = 0; i < newData.length; i++) {
+        let name = `${i + 1}. ${newData[i]}`;
+        result.push(name);
+    }
+    return result;
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (data) => {
+    return data.sort();
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (data) => {
+    return data.reverse();
+};
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
-  console.log(sorter?.(names, sortAscending)?.join("\n"));
-  console.log(sorter?.(names, sortDescending)?.join("\n"));
+    console.log(sorter?.(names, sortAscending)?.join("\n"));
+    console.log(sorter?.(names, sortDescending)?.join("\n"));
 })();
 
 module.exports = {
-  sorter,
-  sortAscending,
-  sortDescending,
-  names,
+    sorter,
+    sortAscending,
+    sortDescending,
+    names,
 };
